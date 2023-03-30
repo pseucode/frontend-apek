43a212939c9b9311b35341b39bf7878e39c61a3a
pseucode <fathurenggaryansyah.gmail.com>
2023-03-30 10:18:30 +0700

    update 2

--- routes.js
+++ routes.js
@@ -3,14 +3,14 @@ const routes = [
       method: '*',
       path: '/',
       handler: (request, h) => {
-          return 'Halaman tidak dapat diakses dengan method tersebut';
+          return 'Halaman tidak dapat diakses dengan method tersebut - Update 3';
       },
   },
   {
       method: 'GET',
       path: '/',
       handler: (request, h) => {
-          return 'Homepage';
+          return 'Homepage - Update 2';
       },
   },
 ];
